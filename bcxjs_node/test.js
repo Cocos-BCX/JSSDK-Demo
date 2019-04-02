8e3f3d8068cf2cb90c0fe67ac9a1c4e9e8b86e32
thiscss <623023808@qq.com>
2019-04-02 16:11:56 +0800

    release v1.3.3.0

--- bcxjs_node/test.js
+++ bcxjs_node/test.js
@@ -6,15 +6,15 @@ var url = require("url");
 var querystring = require('querystring')
 
 let bcx=new BCX({
-    default_ws_node:"ws://47.93.62.96:8050",
+    default_ws_node:"ws://47.93.62.96:8020",
     ws_node_list:[
-        {url:"ws://47.93.62.96:8050",name:"cocos新链"}
+        {url:"ws://47.93.62.96:8020",name:"cocos新链"}
     ],
     networks:[{
         core_asset:"COCOS",
-        chain_id:"53b98adf376459cc29e5672075ed0c0b1672ea7dce42b0b1fe5e021c02bda640" 
+        chain_id:"9fc429a48b47447afa5e6618fde46d1a5f7b2266f00ce60866f9fdd92236e137" 
     }], 
-    faucet_url:"http://47.93.62.96:3000",
+    faucet_url:"http://47.93.62.96:4000",
     auto_reconnect:true                     
 });
 
@@ -44,7 +44,7 @@ let server=http.createServer(function(request, response) {
             response.end();
         })
     } 
-}).listen(8888);
+}).listen(9999);
 
 bcx.subscribeToRpcConnectionStatus({
     callback:status=>{
