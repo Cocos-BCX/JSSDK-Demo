91347b0c28cffa1401b8f27403aa8b8201e46ee0
thiscss <623023808@qq.com>
2019-06-04 18:21:08 +0800

    release -v1.4.18

--- bcxjs_node/test.js
+++ bcxjs_node/test.js
@@ -1,21 +1,21 @@
 
 require('./bcx.node.js');
-
 var http = require("http");
 var url = require("url");
 var querystring = require('querystring')
 let bcx=new BCX({
-    default_ws_node:"ws://47.93.62.96:8049",
     ws_node_list:[
-        {url:"ws://47.93.62.96:8049",name:"COCOS3.0节点1"},
-        {url:"ws://39.106.126.54:8049",name:"COCOS3.0节点2"}
+        {url:"ws://39.97.110.222:8040",name:"Cocos - China - Beijing"},   
+        {url:"ws://47.93.62.96:8049",name:"Cocos - China - Xiamen"} ,
     ],
-    networks:[{
-        core_asset:"COCOS",
-        chain_id:"b9e7cee4709ddaf08e3b7cba63b71c211c845e37c9bf2b865a7b2a592c8adb28" 
-    }], 
-    faucet_url:"http://47.93.62.96:8041",
-    auto_reconnect:true,
+    networks:[
+        {
+            core_asset:"COCOS",
+            chain_id:"7d89b84f22af0b150780a2b121aa6c715b19261c8b7fe0fda3a564574ed7d3e9" 
+        }
+    ], 
+    faucet_url:"http://47.93.62.96:3000",
+    auto_reconnect:false,
     check_cached_nodes_data:false                     
 });
 
